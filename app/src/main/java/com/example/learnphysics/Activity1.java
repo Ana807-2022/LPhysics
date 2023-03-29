754f3652c97e668458f9d68cc65cd5bba6fc5866
Ana807-2022 <areg8072019@gmail.com>
2023-03-29 22:50:09 +0400

    project

--- app/src/main/java/com/example/learnphysics/Activity1.java
+++ app/src/main/java/com/example/learnphysics/Activity1.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class Activity1 extends AppCompatActivity {
     Button back;
@@ -15,6 +16,7 @@ public class Activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+
         back = (Button) findViewById(R.id.back1);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
